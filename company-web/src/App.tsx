1bf19727e5f38f96b4ec4a5d7d1b5fe38bd153cb
98Ganesham <98ganesham@gmail.com>
2023-07-11 21:56:14 +0630

    Nav with responsive

--- company-web/src/App.tsx
+++ company-web/src/App.tsx
@@ -1,51 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
-import Team from "./Pages/Team";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./GlobalStyle";
-
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Team from "./Pages/Team/Team";
+import Contact from "./Pages/Contact/Contact";
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+import NavBar from "./Components/navbar/NavBar";
 
 const App = () => {
-  const theme = {
-    colors: {
-      heading: "rgb(24 24 29)",
-      text: "rgb(24 24 29)",
-      white: "#fff",
-      black: "#000",
-      helper: "#8490ff",
-      darkgreen: "#006400",
-      darkpink: "#aa3364a",
-      bg: "rgba(249 249 255 0.2)",
-      footer_bg: "#0a1435",
-      btn: "rgb(98 84 243)",
-      border: "rgba(98 84 243 0.5)",
-      hr: "#fff",
-      gradient: "linear-gradient(0deg, rgb(132 123 29 )0%, rgb(29 29 24)100%)",
-      shadow:
-        "rgba(0,0,0,0.2) 0px 1px 3px 0px ,rgba(27, 31, 35, 0.15) 0px 0px 0px 10px",
-      shadowSupport: "rgba(0 0 0 0.16) 0px 1px 4px",
-    },
-    media: { mobile: "720px", tap: "1024px" },
-  };
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
+    <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
+        <NavBar/>
+        
+          <Home />
+          <About />
+          <Team />
+          <Contact />
+        
       </BrowserRouter>
-    </ThemeProvider>
-  );
-};
+    </div>
+  )
+}
 
-export default App;
+export default App
--- company-web/src/Elements/Button.tsx
+++ company-web/src/Elements/Button.tsx
@@ -1,52 +1,9 @@
-import styled from "styled-components";
-
-export const Button= styled.button`
-text-decoration: none;
-
-text-aling: center;
-font-size: 2rem;
-max-width: auto;
-padding: 1.4rem 2.4rem ;
-border: none;
-border-radius:5px;
-cursor: pointer;
-background-color:green ;
-
-transition: all 0.3s ease 0s;
--webkit-transition: all 0.3s ease 0s;
--moz-transition: all 0.3s ease 0s;
--o-transition: all 0.3s ease 0s;
 
 
-
-&:hover, 
-&:active{
-    background-color:#93f9b9 ;
-    color: #fff;
-    box-shadow: 0 2rem 2rem 0 rgb(120 144 255 /10% );
-    box-shadow: ${({theme})=> theme.colors.shadowSupport};
-    transform: scale(0.96);
-    
-}
-a{
-    text-decoration: none;
-   color:#000;
-    font-size: 1.5rem;
-    padding-right: 4px;
-
-    
- ]
- 
- }
- .react-logo{
-    display: inline-block;
-    margin-right: 10px;
-    border-radius: 20px;
-    max-height: 20px;
-    max-width: 15px;
-
+const Button = () => {
+  return (
+    <div>Button</div>
+  )
 }
 
-
-
-`;
+export default Button
